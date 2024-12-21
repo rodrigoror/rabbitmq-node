9303fb11f47cb592c4a4482f18cdb68cef6e153e
rodrigoror <rodrigoror@gmail.com>
2024-12-21 09:47:19 -0300

    feat: expressjaeger implemented

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+process.env.JAEGER_COLLECTOR_ENDPOINT = "http://localhost:14268/api/traces";
+process.env.JAEGER_SERVICE_NAME = "jaeger";
+process.env.JAEGER_AGENT_HOST = "localhost";
+process.env.JAEGER_AGENT_PORT = "6831";
+const PORT = process.env.PORT || 3000;
 
+const { track } = require("express-jaeger");
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,12 +15,11 @@ const taskController = require('./controllers/taskController');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(track("/system/startup"));
 
+app.get('/', track("/showTasks"), taskController.showTasks);
+app.get('/add', track("/add"), (req, res) => res.render('addTask'));
+app.post('/add', track("/add"), taskController.addTask);
+app.get('/remove/:id', track("/remove"), taskController.removeTask);
 
-app.get('/', taskController.showTasks);
-app.get('/add', (req, res) => res.render('addTask'));
-app.post('/add', taskController.addTask);
-app.get('/remove/:id', taskController.removeTask);
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
